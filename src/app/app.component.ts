b3967e4034a7aff623e3c6bed83c71e3b0b16d8d
Itokiana <itokianarajoelison@gmail.com>
2023-04-15 01:56:23 +0300

    📦 NEW: Add ng-zorro as UI framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NzI18nService } from 'ng-zorro-antd/i18n';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test1_collaboration_capital';
+  date = null;
+
+  constructor(private i18n: NzI18nService) {}
+
+  onChange(result: Date): void {
+    console.log('onChange: ', result);
+  }
 }
